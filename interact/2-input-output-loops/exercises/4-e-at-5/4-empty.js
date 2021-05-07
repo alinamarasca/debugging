5f92cb4744ef5b0839b315d9f18b109165cef800
HYF <alinamarasca@gmail.com>
2021-05-07 13:31:12 +0200

    a function

--- interact/2-input-output-loops/exercises/4-e-at-5/4-empty.js
+++ interact/2-input-output-loops/exercises/4-e-at-5/4-empty.js
@@ -1,6 +1,39 @@
 "use strict";
 
+let input = '';
+function getPrompt() {
+  input = prompt('enter word where 5th letter is "e" upper or lower case');
+  return input;
+  }
 
+function timeForInput(input) {
+  let answer = '';
+  input = input.toLowerCase();
+  input = input.split(""); //START THE LOOP!!!!!!!!
+  let loopy = false;
+  while (!loopy)
+    if(input[4] == 'e'){
+    answer = ('that\'s RIGHT');
+    console.log(answer);   
+    return ('YES');
+        
+  } else {
+    input = prompt('WRONG, try again')
+    input = input.toLowerCase();
+    input = input.split("");
+    //answer = ('that\'s WRONG');
+    //console.log(answer);
+    //return answer; 
+    continue;
+  }
+  //return ('NO');
+}
+
+getPrompt();
+timeForInput(input);
+//we need to make re-ask we we gave wrong input
+
+/*
 let input = '';
 function getPrompt() {
   input = prompt('enter word where 5th letter is "e" upper or lower case');
@@ -29,3 +62,4 @@ function timeForInput(input) {
 getPrompt();
 timeForInput(input);
 //we need to make re-ask we we gave wrong input
+*/
